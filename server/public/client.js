7981baf8f0ca86c86277bb74a5004e6ee3ecd9a9
Spencer Faust <faus0059@umn.edu>
2019-02-07 15:48:44 -0600

    reversed some things

---
+++ server/public/client.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    console.log('JQ initialized');
+    
+});
+
+
+console.log('JS initialized');
--- server/server.js
+++ server/server.js
@@ -5,9 +5,9 @@ const PORT = 5000;
 
 app.use(express.static('server/public'));
 
-app.use(bodyParser.urlencoder({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.listen(PORT, () => {
     console.log('Listening on port', PORT);
-    
-})
+});
+
